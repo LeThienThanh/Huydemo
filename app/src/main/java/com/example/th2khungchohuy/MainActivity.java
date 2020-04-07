57f39f4380a9424ac5cf8289f53c753f763a85e5
LeThienThanh <lecungtienzz@gmail.com>
2020-04-07 15:39:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/th2khungchohuy/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.th2khungchohuy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    DatabaseReference mref;
+    StorageReference msto;
+
+    ImageView imgclothe, imgcustom, imgtest;
+    Button btnclothe, btncustom, btntest;
+    private static final int    CUSTOM_IMG= 1;
+    private static final int    CLOTHE_IMG= 2;
+    Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imgclothe = findViewById(R.id.imgclothe);
+        imgcustom = findViewById(R.id.imgcustom);
+        imgtest = findViewById(R.id.imgtestclothe);
+
+        btnclothe = findViewById(R.id.btnclothe);
+        btncustom = findViewById(R.id.btncustom);
+        btntest = findViewById(R.id.btntestclothe);
+
+        mAuth = FirebaseAuth.getInstance();
+        mref = FirebaseDatabase.getInstance().getReference();
+        msto = FirebaseStorage.getInstance().getReference();
+
+
+        btncustom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                MainActivity.this.startActivityForResult(intent, CUSTOM_IMG);
+            }
+        });
+
+        btnclothe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                MainActivity.this.startActivityForResult(intent, CLOTHE_IMG);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+
+
+            if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            } else if (requestCode == CUSTOM_IMG){
+                imageUri = data.getData();
+                Picasso.get().load(imageUri.toString()).into(imgcustom);
+                upload("custom",imgcustom);
+            } else if (requestCode == CLOTHE_IMG){
+                imageUri = data.getData();
+                Picasso.get().load(imageUri.toString()).into(imgclothe);
+                upload("clothe",imgclothe);
+            }
+        }
+
+
+    }
+    public void upload(String type, ImageView img){
+        img.setDrawingCacheEnabled(true);
+        img.buildDrawingCache();
+
+        Bitmap bitmap = img.getDrawingCache();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] data = byteArrayOutputStream.toByteArray();
+
+        final StorageReference filepath = msto.child(type).child(imageUri.getLastPathSegment());
+
+        filepath.putBytes(data).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(MainActivity.this,"Post successful", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,"Post Fail", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
